3004491ef30fa5abd4488b66223ed93585bdcad2
Giacomo Cariello <g.cariello@ieee.org>
2017-11-21 23:07:40 +0100

    Updated dependencies. Added support for input/output attributes.

--- provider/resource.go
+++ provider/resource.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
@@ -12,10 +13,12 @@ import (
 	"strconv"
 	"strings"
 	"syscall"
-        "time"
+	"time"
 
+	"github.com/hashicorp/hcl"
 	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/spf13/viper"
+	toml "github.com/pelletier/go-toml"
+	"gopkg.in/yaml.v2"
 )
 
 func init() {
@@ -33,13 +36,13 @@ func resourceRunCommand() *schema.Resource {
 			"apply": {
 				Type:     schema.TypeString,
 				Required: true,
-                                ForceNew: true,
+				ForceNew: true,
 			},
 
 			"check": {
 				Type:     schema.TypeString,
 				Required: true,
-                                ForceNew: true,
+				ForceNew: true,
 			},
 
 			"destroy": {
@@ -50,7 +53,7 @@ func resourceRunCommand() *schema.Resource {
 			"shell": {
 				Type:     schema.TypeString,
 				Required: true,
-                                ForceNew: true,
+				ForceNew: true,
 				DefaultFunc: func() (interface{}, error) {
 					if runtime.GOOS == "windows" {
 						return "cmd /C", nil
@@ -66,12 +69,51 @@ func resourceRunCommand() *schema.Resource {
 				Default:  0,
 			},
 
-			"output_format": {
+			"check_input_format": {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
 
-			"output": {
+			"check_inputs": {
+				Type: schema.TypeMap,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Optional: true,
+			},
+
+			"apply_input_format": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"apply_inputs": {
+				Type: schema.TypeMap,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Optional: true,
+			},
+
+			"destroy_input_format": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"destroy_inputs": {
+				Type: schema.TypeMap,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Optional: true,
+			},
+
+			"check_output_format": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"check_outputs": {
 				Type:     schema.TypeMap,
 				Computed: true,
 			},
@@ -82,11 +124,15 @@ func resourceRunCommand() *schema.Resource {
 func makeCommand(key string, d *schema.ResourceData) *exec.Cmd {
 	argv := strings.Split(d.Get("shell").(string), " ")
 	argv = append(argv, d.Get(key).(string))
-	log.Printf("[DEBUG] Built command-line argv: %+v", argv)
+	log.Printf("[DEBUG] Running command: %s", strings.Join(argv, " "))
 	return exec.Command(argv[0], argv[1:]...)
 }
 
-func runCommand(cmd *exec.Cmd) error {
+func runCommand(cmd *exec.Cmd, input []byte) error {
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return err
@@ -97,6 +143,15 @@ func runCommand(cmd *exec.Cmd) error {
 	}
 
 	done := make(chan bool, 1)
+	go func() {
+		defer stdin.Close()
+		if len(input) > 0 {
+			_, err := stdin.Write(input)
+			if err != nil {
+				log.Printf("[WARN] error writing to stdin: %s", err.Error())
+			}
+		}
+	}()
 	go func() {
 		scanner := bufio.NewScanner(stdout)
 		for scanner.Scan() {
@@ -124,10 +179,78 @@ func runCommand(cmd *exec.Cmd) error {
 	return nil
 }
 
+func makeInput(d *schema.ResourceData, inputType string) ([]byte, error) {
+	inputFormat := d.Get(fmt.Sprintf("%s_input_format", inputType)).(string)
+	inputs := d.Get(fmt.Sprintf("%s_inputs", inputType)).(map[string]interface{})
+	switch strings.ToLower(inputFormat) {
+	case "yaml", "yml":
+		return yaml.Marshal(inputs)
+	case "json":
+		return json.Marshal(inputs)
+	case "toml":
+		return toml.Marshal(inputs)
+	case "", "stdin":
+		if v, ok := inputs["stdin"]; ok {
+			return v.([]byte), nil
+		}
+	default:
+		log.Printf("[WARN] Unsupported %s_input_format: %s", inputType, inputFormat)
+	}
+	return []byte{}, nil
+}
+
+func setOutput(d *schema.ResourceData, buf *bytes.Buffer, outputType string) error {
+	outputFormat := d.Get(fmt.Sprintf("%s_output_format", outputType)).(string)
+	outputs := make(map[string]interface{})
+	switch strings.ToLower(outputFormat) {
+	case "yaml", "yml":
+		if err := yaml.Unmarshal(buf.Bytes(), &outputs); err != nil {
+			return err
+		}
+
+	case "json":
+		if err := json.Unmarshal(buf.Bytes(), &outputs); err != nil {
+			return err
+		}
+
+	case "hcl":
+		obj, err := hcl.Parse(string(buf.Bytes()))
+		if err != nil {
+			return err
+		}
+		if err = hcl.DecodeObject(&outputs, obj); err != nil {
+			return err
+		}
+
+	case "toml":
+		tree, err := toml.LoadReader(buf)
+		if err != nil {
+			return err
+		}
+		tmap := tree.ToMap()
+		for k, v := range tmap {
+			outputs[k] = v
+		}
+	case "", "stdout":
+		outputs["stdout"] = string(buf.Bytes())
+	case "null":
+		outputs = nil
+	default:
+		log.Printf("[WARN] Unsupported output_format for resource \"%s\"", d.Id())
+		outputs = nil
+	}
+	log.Printf("[DEBUG] check_outputs = \"%+v\"", outputs)
+	return d.Set("check_outputs", outputs)
+}
+
 func resourceRunCommandApply(d *schema.ResourceData, meta interface{}) error {
+	input, err := makeInput(d, "apply")
+	if err != nil {
+		return err
+	}
 	cmd := makeCommand("apply", d)
 
-	if err := runCommand(cmd); err != nil {
+	if err := runCommand(cmd, input); err != nil {
 		return err
 	}
 
@@ -135,8 +258,16 @@ func resourceRunCommandApply(d *schema.ResourceData, meta interface{}) error {
 }
 
 func resourceRunCommandCheck(d *schema.ResourceData, meta interface{}) error {
+	input, err := makeInput(d, "check")
+	if err != nil {
+		return err
+	}
 	cmd := makeCommand("check", d)
 
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return err
@@ -149,9 +280,20 @@ func resourceRunCommandCheck(d *schema.ResourceData, meta interface{}) error {
 	stdoutCh := make(chan []byte)
 	done := make(chan bool, 1)
 	go func() {
+		defer stdin.Close()
+		if len(input) > 0 {
+			_, err := stdin.Write(input)
+			if err != nil {
+				log.Printf("[WARN] error writing to stdin: %s", err.Error())
+			}
+		}
+	}()
+	go func() {
 		scanner := bufio.NewScanner(stdout)
 		for scanner.Scan() {
-			log.Printf("[INFO] stdout: %s", scanner.Text())
+			out := scanner.Bytes()
+			log.Printf("[INFO] stdout: %s", out)
+			stdoutCh <- out
 		}
 		done <- true
 	}()
@@ -198,40 +340,24 @@ func resourceRunCommandCheck(d *schema.ResourceData, meta interface{}) error {
 		}
 	}
 
-	if exitCode != d.Get("exit_code").(int) {
-		d.Set("exit_code", exitCode)
-		return nil
-	}
-
 	if d.Id() == "" {
 		d.SetId(fmt.Sprintf("%d", rand.Int()))
 	}
 
-	outputFormat := d.Get("output_format").(string)
-        outputMap := make(map[string]string)
-	if outputFormat != "" {
-		if stringInSlice(outputFormat, viper.SupportedRemoteProviders) {
-			output := viper.New()
-			output.SetConfigType(outputFormat)
-			output.ReadConfig(&buf)
-			if err := output.Unmarshal(&outputMap); err != nil {
-				return err
-			}
-			d.Set("output", outputMap)
-		} else if outputFormat == "null" {
-                        outputMap = nil
-		} else {
-			log.Printf("[WARN] Unsupported output_format for resource \"%s\"", d.Id())
-                        outputMap = nil
-		}
-	} else {
-                outputMap["stdout"] = string(buf.Bytes())
+	if exitCode != d.Get("exit_code").(int) {
+		d.Set("exit_code", exitCode)
+		d.Set("check_outputs", nil)
+		return nil
 	}
-	d.Set("output", outputMap)
-	return nil
+
+	return setOutput(d, &buf, "check")
 }
 
 func resourceRunCommandDestroy(d *schema.ResourceData, meta interface{}) error {
+	input, err := makeInput(d, "destroy")
+	if err != nil {
+		return err
+	}
 	cmd := makeCommand("destroy", d)
-	return runCommand(cmd)
+	return runCommand(cmd, input)
 }
